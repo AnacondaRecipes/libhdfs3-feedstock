a426f77a757310fb8e76bb6c853946159f0f2c0c
Ray Donnelly <mingw.android@gmail.com>
2018-07-06 15:25:35 -0400

    Add PPC support

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,17 @@
 
 mkdir build
 cd build
-../libhdfs3/bootstrap --prefix=$PREFIX --dependency=$PREFIX
-make -j${CPU_COUNT}
+if [[ ${target_platform} == linux-ppc64le ]]; then
+  cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+        -DCMAKE_PREFIX_PATH=${PREFIX} \
+        -DENABLE_BOOST=ON \
+        -DENABLE_COVERAGE=OFF \
+        -DENABLE_LIBCPP=OFF \
+        -DENABLE_SSE=OFF \
+        ../libhdfs3
+else
+  ../libhdfs3/bootstrap --prefix=$PREFIX --dependency=$PREFIX
+fi
+make -j${CPU_COUNT} ${VERBOSE_CM}
 make install
 rm $PREFIX/lib/libhdfs3.a  # don't need static
