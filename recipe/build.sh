fc9ba9a128f3ff4b3ff6ac5caa8e14f92f49ed3b
Tobias Koch <tkoch@anaconda.com>
2021-12-30 15:09:33 +0300

    Per-arch CMake parameters

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,18 +1,37 @@
-#!/usr/bin/env bash
+#!/bin/bash -e
 
 mkdir build
 cd build
-if [[ ${target_platform} == linux-ppc64le ]]; then
-  cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-        -DCMAKE_PREFIX_PATH=${PREFIX} \
-        -DENABLE_BOOST=ON \
-        -DENABLE_COVERAGE=OFF \
-        -DENABLE_LIBCPP=OFF \
-        -DENABLE_SSE=OFF \
-        ../libhdfs3
+
+CMAKE_OPTS=""
+
+case ${target_platform} in
+  linux-ppc64le)
+    CMAKE_OPTS="
+      -DENABLE_BOOST=ON \
+      -DENABLE_COVERAGE=OFF
+      -DENABLE_LIBCPP=OFF
+      -DENABLE_SSE=OFF
+    "
+    ;;
+  linux-aarch64)
+    CMAKE_OPTS="
+      -DENABLE_SSE=OFF
+    "
+    ;;
+esac
+
+if [ -n "$CMAKE_OPTS" ]; then
+  cmake \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+    -DCMAKE_PREFIX_PATH=${PREFIX} \
+    $CMAKE_OPTS \
+    ../libhdfs3
 else
   ../libhdfs3/bootstrap --prefix=$PREFIX --dependency=$PREFIX
 fi
+
 make -j${CPU_COUNT} ${VERBOSE_CM}
 make install
+
 rm $PREFIX/lib/libhdfs3.a  # don't need static
