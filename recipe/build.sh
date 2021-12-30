93cdcf48b7d0428cd4b49a609cb5391e3211433f
Tobias Koch <tkoch@anaconda.com>
2021-12-30 17:20:21 +0300

    More consistent quoting and variable references

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,35 +3,36 @@
 mkdir build
 cd build
 
-CMAKE_OPTS=""
+CMAKE_RECIPE_ARGS=""
 
 case ${target_platform} in
   linux-ppc64le)
-    CMAKE_OPTS="
-      -DENABLE_BOOST=ON \
+    CMAKE_RECIPE_ARGS="
+      -DENABLE_BOOST=ON
       -DENABLE_COVERAGE=OFF
       -DENABLE_LIBCPP=OFF
       -DENABLE_SSE=OFF
     "
     ;;
   linux-aarch64)
-    CMAKE_OPTS="
+    CMAKE_RECIPE_ARGS="
       -DENABLE_SSE=OFF
     "
     ;;
 esac
 
-if [ -n "$CMAKE_OPTS" ]; then
+if [ -n "${CMAKE_RECIPE_ARGS}" ]; then
   cmake \
-    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-    -DCMAKE_PREFIX_PATH=${PREFIX} \
-    $CMAKE_OPTS \
+    -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+    -DCMAKE_PREFIX_PATH="${PREFIX}" \
+    ${CMAKE_ARGS} \
+    ${CMAKE_RECIPE_ARGS} \
     ../libhdfs3
 else
-  ../libhdfs3/bootstrap --prefix=$PREFIX --dependency=$PREFIX
+  ../libhdfs3/bootstrap --prefix="${PREFIX}" --dependency="${PREFIX}"
 fi
 
 make -j${CPU_COUNT} ${VERBOSE_CM}
 make install
 
-rm $PREFIX/lib/libhdfs3.a  # don't need static
+rm "${PREFIX}/lib/libhdfs3.a"  # don't need static
